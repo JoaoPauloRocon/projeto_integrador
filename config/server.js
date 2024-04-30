0836fe986d05199806625d30c4080b97625a1e58
Luiza <luizaklug1001@gmail.com>
2024-04-30 20:08:20 -0300

    criado o servidor
    feito a criação do servidor

---
+++ config/server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser  = require('body-parser')
+var consign = require('consign');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
+const multer = require('multer');
+const configMulter = multer({
+    storage: multer.diskStorage({
+        destination: function(req,file,callback){
+            callback(null, './public/uploads');
+        },
+        filename: function(req,file,callback){
+            callback(null, Date.now() + '-' + file.originalname);
+        }
+    })
+});
+
+app.upload = configMulter;
+app.set('view engine', 'ejs');
+app.set('views', './app/views')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(expressValidator());
+app.use(express.static('public'));
+app.use(expressSession({
+    secret: 'fdghfhdghuhd',
+    resave: false,
+    saveUninitialized: false
+}));
+
+consign()
+    .include('app/routes')
+    .then('config/dbConnection.js')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+module.exports = app;
