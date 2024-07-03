e699297930ebfc1718aa1e0f0f5c32dd34c800f2
João Paulo Rocon <luizaklug1001@gmail.com>
2024-07-03 08:19:51 -0300

    Validações de login
    Feito validações de login do usuário

--- app/routes/login/login_usuario.js
+++ app/routes/login/login_usuario.js
@@ -3,9 +3,17 @@ module.exports = function(app) {
         app.app.controllers.login.login.busca_login(app,req, res)
     });
 
+    app.get('/cadastrar', (req, res) => {
+        app.app.controllers.login.login.cadastrar_usuario(app,req, res)
+    });
+
     app.post('/cadastro_usuarios', function(req, res){
         console.log('chego aq')
         app.app.controllers.login.login.salvar_usuario(app, req, res);
+    });
+
+    app.post('/valida_login', function(req, res){
+        app.app.controllers.login.login.valida_login(app, req, res);
     });
 };
 
