756c0f68276322be70ef5faa854d8418d893a593
João Paulo Rocon <104106279+JoaoPauloRocon@users.noreply.github.com>
2024-09-19 15:26:14 -0300

    Revert "Fazer pagina sobre"

--- app/controllers/sobre/sobre.js
+++
@@ -1,3 +0,0 @@
-module.exports.renderiza_sobre = function(app, req, res) {
-    res.render("sobre/sobre", { flagAdmin: req.session.autorizado || null, codLogado: req.session.codLogado || null });
-};
--- app/routes/sobre/sobre.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-    app.get('/sobre', (req, res) => {
-        app.app.controllers.sobre.sobre.renderiza_sobre(app,req, res)
-    });
-};
